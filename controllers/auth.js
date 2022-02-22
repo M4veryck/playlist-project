e158f486349042c963d0eb989cc775132a23b7e6
M4veryck <mmaya@unal.edu.co>
2022-02-21 23:13:37 -0500

    added bcryptjs to hash passwords and improve security, also made  minor improvements in controllers/auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,11 @@
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+let createToken = (user) =>
+  jwt.sign({ userId: user._id, name: user.name }, process.env.JWT_SECRET, {
+    expiresIn: '1d',
+  })
 
 login = async (req, res) => {
   const { email, password } = req.body
@@ -10,24 +16,20 @@ login = async (req, res) => {
   if (!user) {
     throw new Error()
   }
-  if (user.password !== password) {
+  if (bcrypt.compareSync(password, user.password) === false) {
     throw new Error()
   }
-  const token = jwt.sign(
-    { userId: user._id, name: user.name },
-    process.env.JWT_SECRET,
-    { expiresIn: '1d' }
-  )
+  const token = createToken(user)
   res.status(200).json({ user: user.name, token: token })
 }
 
 register = async (req, res) => {
-  const user = await User.create({ ...req.body })
-  const token = jwt.sign(
-    { userId: user._id, name: user.name },
-    process.env.JWT_SECRET,
-    { expiresIn: '1d' }
+  const hashedPassword = await bcrypt.hash(
+    req.body.password,
+    bcrypt.genSaltSync(12)
   )
+  const user = await User.create({ ...req.body, password: hashedPassword })
+  const token = createToken(user)
   res.status(201).json({ user: user.name, token: token })
 }
 
