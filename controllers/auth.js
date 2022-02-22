b3a1979b9d3cfa38b8b26774db793645e6616fb2
M4veryck <mmaya@unal.edu.co>
2022-02-21 19:21:23 -0500

    login and register controllers modified: now they use tokens (JWT) and return them

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,34 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
-login = (req, res) => {
-  res.send('This is the login')
+login = async (req, res) => {
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new Error()
+  }
+  const user = await User.findOne({ email: email })
+  if (!user) {
+    throw new Error()
+  }
+  if (user.password !== password) {
+    throw new Error()
+  }
+  const token = jwt.sign(
+    { userId: user._id, name: user.name },
+    process.env.JWT_SECRET,
+    { expiresIn: '1d' }
+  )
+  res.status(200).json({ user: user.name, token: token })
 }
 
 register = async (req, res) => {
   const user = await User.create({ ...req.body })
-  res.status(201).json(user)
+  const token = jwt.sign(
+    { userId: user._id, name: user.name },
+    process.env.JWT_SECRET,
+    { expiresIn: '1d' }
+  )
+  res.status(201).json({ user: user.name, token: token })
 }
 
 module.exports = {
