7221e010deb13e58c06bb972bbfe227b1574edf6
M4veryck <mmaya@unal.edu.co>
2022-02-23 18:27:27 -0500

    v 1.0: playlists-app
    Add security features in app.js
    Polish some code in controllers/auth.js
    v 1.0 has auth and playlists APIs, both with security features

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const connectDB = require('./db/connect')
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
 
 require('express-async-errors')
 require('dotenv').config()
@@ -13,7 +17,21 @@ const playlistsRouter = require('./routes/playlists')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const authMiddleware = require('./middleware/auth')
+
+app.set('trust proxy', 1)
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  })
+)
+
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 app.get('/', (req, res) => {
   res.send('This is the home page')
@@ -26,7 +44,7 @@ app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
 // server start
-const port = 5000
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
